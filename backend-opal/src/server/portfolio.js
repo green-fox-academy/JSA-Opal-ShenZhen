58f7dc876b3a01a066cf7a8bd32d6215d5d95456
Layla Dai <Layla_Dai@epam.com>
2019-11-14 14:03:39 +0800

    this commit will complete mock list portfolio endpoint

--- backend-opal/src/server/portfolio.js
+++ backend-opal/src/server/portfolio.js
@@ -3,8 +3,25 @@ const portfolioList = [
 ]
 
 exports.getPortfolio = function (server) {
-    if (req.headers.authorization == 'Bearer token') {
-        server.get('/portfolio', function (req, res) {
+    server.get('/portfolio', function (req, res) {
+        let authorization = req.headers.authorization;
+        if (!authorization || !authorization.startsWith('Bearer')) {
+            var response = {
+                "error": 'Authentication header is missing!'
+            };
+
+            res.statusCode = 401;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(response));
+        } else if (!authorization.includes('token')) {
+            var response = {
+                "error": 'User is not allowed to do this action!'
+            };
+
+            res.statusCode = 403;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(response));
+        } else {
             var response = {
                 "portfolio": portfolioList
             };
@@ -12,6 +29,6 @@ exports.getPortfolio = function (server) {
             res.statusCode = 200;
             res.setHeader('Content-Type', 'application/json');
             res.end(JSON.stringify(response));
-        })
-    }
+        }
+    })
 }
