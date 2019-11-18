b5c90f60fe963e62f11eea15e7273f8e02aa83d9
Yuan-EPAM <Yuan_Liao@epam.com>
2019-11-18 14:27:22 +0800

    add test(jest) files

---
+++ frontend-opal/__tests__/components/portfolio/Allocation/Allocation.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import AllocationInfo from '../../../../components/portfolio/AllocationInfo';
+
+let pieData = [
+    { x: "IT", y: 40, label: "40%" },
+    { x: "Finance", y: 30, label: "30%" },
+    { x: "Energy", y: 30, label: "30%" }
+]
+let legendData = [
+    { name: "IT", symbol: { type: "square" } },
+    { name: "Finance", symbol: { type: "square" } },
+    { name: "Energy", symbol: { type: "square" } }
+]
+const pieColor = ["#711702", "#A91600", "#E12C00"]
+
+describe('<Header />', () => {
+    it('renders correctly', () => {
+        const tree = renderer
+            .create(<AllocationInfo
+                pieData={pieData}
+                pieColor={pieColor}
+                legendData={legendData}
+            />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
