a61f6d66ae09a3b47100a8cd796f8a7f19d8f0d6
martinupset <Martin_Ma@epam.com>
2019-11-18 14:42:27 +0800

    setup pm2

---
+++ backend-opal/ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'opal',
+    script: './main.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-54-199-209-203.ap-northeast-1.compute.amazonaws.com',
+      key: 'C:/Users/Martin_Ma/Desktop/jsa-opal.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:green-fox-academy/JSA-Opal-ShenZhen.git',
+      path: '/home/ubuntu/opal',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
