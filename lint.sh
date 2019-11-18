3a3ddf10c270347f6269005e1b289e2b2cc96d7c
hondahmd <1535307545@qq.com>
2019-11-18 13:17:26 +0800

    delete comment lines

--- lint.sh
+++ lint.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# FILES_CHANGED=$(git diff --name-only HEAD~ HEAD | grep '.js')
-# $(./node_modules/eslint/bin/eslint.js ${FILES_CHANGED} --ext .js --fix)
 cd backend-opal && npm run lint
 cd .. && cd frontend-opal && npm run lint 
