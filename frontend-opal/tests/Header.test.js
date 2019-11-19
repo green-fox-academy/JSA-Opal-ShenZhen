342e37cc3672c1bc52b0110c3933fb01b545784b
Yuan-EPAM <Yuan_Liao@epam.com>
2019-11-19 14:35:43 +0800

    update indent from 4 to 2

--- frontend-opal/tests/Header.test.js
+++ frontend-opal/tests/Header.test.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Header from '../components/Header'
-import { describe } from 'jest-expo';
-
+import Header from '../components/Header';
 
 describe('<Header />', () => {
-    it('renders correctly', () => {
-        const tree = renderer.create(<Header />).toJSON();
-        expect(tree).toMatchSnapshot();
-    });
+  it('renders correctly', () => {
+    const tree = renderer.create(<Header />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
