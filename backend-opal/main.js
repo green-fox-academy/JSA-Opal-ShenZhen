513bbe0c6047c0720713d8d05ee7e96009b4a181
Yuan-EPAM <Yuan_Liao@epam.com>
2019-11-19 16:25:04 +0800

    refactor original main.js

--- backend-opal/main.js
+++ backend-opal/main.js
@@ -4,9 +4,11 @@ const express = require('express');
 const server = express();
 const PORT = 3001;
 
+const getPortfolio = require('./src/server/portfolio');
 const getWatchLists = require('./src/server/watchLists');
 
 // wait for handlers
+server.use('/portfolio', getPortfolio);
 server.use('/watchlists', getWatchLists);
 
 server.listen(PORT, () => {
