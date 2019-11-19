e8fb38a6e950ba8b951dec9d3bcaf773d1fd9a45
Yuan-EPAM <Yuan_Liao@epam.com>
2019-11-19 16:14:50 +0800

    refactor files (undo deleted files)

---
+++ backend-opal/src/server/portfolio.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const authService = require('./common/authorizationService');
+
+const portfolioList = [{ id: '1', userId: '1', symbol: 'MSFT', amount: 123 }];
+
+router.get('/', function(req, res) {
+  const { authorization } = req.headers;
+  if (authService.checkAuthHeader(authorization)) {
+    const response = {
+      error: 'Authentication header is missing!'
+    };
+
+    res.status(401).send(response);
+  } else if (authService.authorize(authorization)) {
+    const response = {
+      error: 'User is not allowed to do this action!'
+    };
+
+    res.status(403).send(response);
+  } else {
+    const response = {
+      portfolio: portfolioList
+    };
+
+    res.status(200).send(response);
+  }
+});
+
+module.exports = router;
---
+++ backend-opal/src/server/watchLists.test.js
@@ -0,0 +1,27 @@
+const request = require('supertest');
+const server = require('../../main');
+
+describe('Watchlist Endpoint', () => {
+  it('no authorization, should get 401', async () => {
+    const res = await request(server).get('/watchlists');
+    expect(res.statusCode).toEqual(401);
+  });
+  it("authorization equals to 'token', should get 401", async () => {
+    const res = await request(server)
+      .get('/watchlists')
+      .set('Authorization', 'token');
+    expect(res.statusCode).toEqual(401);
+  });
+  it("authorization equals to 'Bearer', should get 403", async () => {
+    const res = await request(server)
+      .get('/watchlists')
+      .set('Authorization', 'Bearer');
+    expect(res.statusCode).toEqual(403);
+  });
+  it("authorization equals to 'Bearer token', should get 200 OK", async () => {
+    const res = await request(server)
+      .get('/watchlists')
+      .set('Authorization', 'Bearer token');
+    expect(res.statusCode).toEqual(200);
+  });
+});
