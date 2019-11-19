aec1c66266b711df23553e528f06a8af9caae743
Yuan-EPAM <Yuan_Liao@epam.com>
2019-11-19 15:00:37 +0800

    implement watchlists endpoint and test file

--- backend-opal/main.js
+++ backend-opal/main.js
@@ -2,14 +2,15 @@ require('dotenv').config();
 const express = require('express');
 
 const server = express();
+const PORT = 3001;
 
-const getPortfolio = require('./src/server/portfolio');
+const getWatchLists = require('./src/server/watchLists');
 
 // wait for handlers
-server.use('/portfolio', getPortfolio);
+server.use('/watchlists', getWatchLists);
 
-server.listen(process.env.SERVER_PORT, () => {
-  console.log(`express server's running on PORT ${process.env.SERVER_PORT}`);
+server.listen(PORT, () => {
+  console.log(`express server's running on PORT ${PORT}`);
 });
 
 module.exports = server;
--- backend-opal/src/server/tests/watchLists.test.js
+++ backend-opal/src/server/tests/watchLists.test.js
@@ -1,26 +1,26 @@
 const request = require('supertest');
-const server = require('../../main');
+const server = require('../../../main');
 
-describe('Portfolio Endpoint', () => {
+describe('Watchlist Endpoint', () => {
   it('no authorization, should get 401', async () => {
-    const res = await request(server).get('/portfolio');
+    const res = await request(server).get('/watchlists');
     expect(res.statusCode).toEqual(401);
   });
   it("authorization equals to 'token', should get 401", async () => {
     const res = await request(server)
-      .get('/portfolio')
+      .get('/watchlists')
       .set('Authorization', 'token');
     expect(res.statusCode).toEqual(401);
   });
   it("authorization equals to 'Bearer', should get 403", async () => {
     const res = await request(server)
-      .get('/portfolio')
+      .get('/watchlists')
       .set('Authorization', 'Bearer');
     expect(res.statusCode).toEqual(403);
   });
   it("authorization equals to 'Bearer token', should get 200 OK", async () => {
     const res = await request(server)
-      .get('/portfolio')
+      .get('/watchlists')
       .set('Authorization', 'Bearer token');
     expect(res.statusCode).toEqual(200);
   });
