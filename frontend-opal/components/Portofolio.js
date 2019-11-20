81487c531b9cd12586b8e5df256857b066420e4f
Layla Dai <Layla_Dai@epam.com>
2019-11-20 17:50:39 +0800

    this commit will upload JSAO-11, portfolio instrument list

--- frontend-opal/components/Portofolio.js
+++ frontend-opal/components/Portofolio.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { View } from 'react-native';
+import { SafeAreaView, ScrollView } from 'react-native';
 import Header from './Header';
 import PortfolioContainer from './PortfolioContainer';
 
 export default function Portfolio() {
   return (
-    <View>
-      <Header />
-      <PortfolioContainer />
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <Header />
+        <PortfolioContainer />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
