7ffc68d02fcda08b2aa232937d9bd5f286652962
hondahmd <mingda.hong@qq.com>
2019-11-21 10:41:36 +0800

    lint json files and optimize watchlist reducer

--- frontend-opal/src/reducers/watchlists.js
+++ frontend-opal/src/reducers/watchlists.js
@@ -1,13 +1,9 @@
 const pairs = new Map();
-pairs.set('GET_ALL_DATA', getAllData);
+pairs.set('GET_ALL_DATA', (state, action) => action.payload.data);
 
 function watchLists(state = {}, action) {
   if (pairs.has(action.type)) return pairs.get(action.type)(state, action);
   return state;
-}
-
-function getAllData(state, action) {
-  return action.payload.data;
 }
 
 export default watchLists;
