6ad31f2a41981caa5e75f2f797eb406a4e4577fb
Yuan-EPAM <Yuan_Liao@epam.com>
2019-11-22 16:05:28 +0800

    delete unnecessary print

--- backend-opal/src/server/stockServer.js
+++ backend-opal/src/server/stockServer.js
@@ -1,7 +1,6 @@
 const dbDriver = require('./db/driver');
 
 const getStockByUser = async userID => {
-  console.log('stockServer');
   const data = await dbDriver.getStockByUserId(userID);
   return data;
 };
