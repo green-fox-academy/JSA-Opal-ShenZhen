7d343f5f24c5609e238b04a13ea57d629bfc4464
Yuan-EPAM <Yuan_Liao@epam.com>
2019-11-22 12:59:29 +0800

    add mock test for stock server

--- backend-opal/src/server/stock.js
+++ backend-opal/src/server/stock.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const MYSQL_DB = require('./db/driver');
 
-const getStock = (userID) =>
+const getStock = userID =>
   router.get(`/stock/:${userID}`, async (req, res) => {
     const data = await MYSQL_DB.getStockByUserId(userID);
 
