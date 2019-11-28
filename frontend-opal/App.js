5812efc36dbc913ddc8fb1e2110c388b9ff46ebe
Yuan Liao <Yuan_Liao@epam.com>
2019-11-28 18:24:51 +0800

    temporary change App entry

--- frontend-opal/App.js
+++ frontend-opal/App.js
@@ -6,13 +6,16 @@ import thunk from 'redux-thunk';
 import reducers from 'reducers/root';
 import Investment from './src/components/investment';
 
+import InstrumentDetail from './src/components/PortfolioContainer/InstrumentDetail';
+
 export default function App() {
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
   return (
     <Provider store={store}>
-      <Investment />
+      {/* <Investment /> */}
+      <InstrumentDetail />
     </Provider>
   );
 }
