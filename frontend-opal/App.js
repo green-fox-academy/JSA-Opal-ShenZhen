47094707dd8dc37b6409b6bea8290fff9f994088
martinupset <Martin_Ma@epam.com>
2019-11-29 12:11:26 +0800

    donot contain app.js

--- frontend-opal/App.js
+++ frontend-opal/App.js
@@ -1,14 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, compose, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import * as Font from 'expo-font';
-import { AppLoading } from 'expo';
-/* eslint-disable import/no-extraneous-dependencies */
-import { Ionicons, MaterialIcons } from '@expo/vector-icons';
-/* eslint-disable import/no-extraneous-dependencies */
-import RobotoFont from 'native-base/Fonts/Roboto.ttf';
-import RobotoMediumFont from 'native-base/Fonts/Roboto_medium.ttf';
 
 import reducers from 'reducers/root';
 import Investment from './src/components/investment';
@@ -17,31 +9,11 @@ export default function App() {
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
-  const [ready, setReady] = useState(false);
+  
 
-  async function loadFonts() {
-    await Font.loadAsync({
-      Roboto: RobotoFont,
-      Roboto_medium: RobotoMediumFont,
-      ...Ionicons.font,
-      ...MaterialIcons.font
-    });
-    setReady(true);
-  }
-
-  useEffect(() => {
-    loadFonts();
-  }, []);
-
-  return (
-    <>
-      {ready ? (
+  return ( 
         <Provider store={store}>
           <Investment />
         </Provider>
-      ) : (
-        <AppLoading />
-      )}
-    </>
   );
 }
